d2f02bb1cac91baf81df8aa65d18c57d4eb19198
Amélie <72796730+aweliego@users.noreply.github.com>
2020-12-01 21:33:34 +0100

    Removed context paragraph

--- mysteriousOrganism.js
+++ mysteriousOrganism.js
@@ -1,5 +1,3 @@
-/* Context: You’re part of a research team that has found a new mysterious organism at the bottom of the ocean near hydrothermal vents. Your team names the organism, Pila aequor (P. aequor), and finds that it is only comprised of 15 DNA bases. The small DNA samples and frequency at which it mutates due to the hydrothermal vents make P. aequor an interesting specimen to study. However, P. aequor cannot survive above sea level and locating P. aequor in the deep sea is difficult and expensive. Your job is to create objects that simulate the DNA of P. aequor for your research team to study. */
-
 /*There are two helper functions: returnRandBase() and mockUpStrand().
 
 DNA is comprised of four bases (Adenine, Thymine, Cytosine, and Guanine). When returnRandBase() is called, it will randomly select a base and return the base ('A','T','C', or 'G').
